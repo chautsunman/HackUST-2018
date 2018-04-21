d87a823bf11eb0e040d135c8e10e55df9c9f1176
stevenTCL <steven351906@yahoo.com.hk>
2018-04-21 19:22:24 +0800

    add contract list

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import MenuAppBar from './AppBar';
+import CheckboxListSecondary from './contactList';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-		<MenuAppBar />
+        <MenuAppBar />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div align="center">
+          <CheckboxListSecondary />
+        </div>
       </div>
     );
   }
