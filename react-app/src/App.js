c1443cc23bcd6957a25ebb154236ca4a830aa64a
Cameron SUEN <cameron.hpsuen@gmail.com>
2018-04-21 18:47:57 +0800

    Set up app bar

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import MenuAppBar from './AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+		<MenuAppBar />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
