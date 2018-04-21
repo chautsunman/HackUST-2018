e1b8fda7265d3e4f99d455fef04a6913db4fffd0
Chau Tsun Man <tsunman.chau.g@gmail.com>
2018-04-21 22:18:15 +0800

    Whiteboard

--- react-app/src/WhiteBoard.js
+++ react-app/src/WhiteBoard.js
@@ -14,91 +14,50 @@ const styles = theme => ({
 	})
 });
 
-const boardStyle = {
-	width: '100%'
-}
-
 class WhiteBoard extends React.Component {
-
 	constructor(props) {
 		super(props);
 		this.el = React.createRef();
-		this.flag = false;
-		this.prevX = 0;
-		this.currX = 0;
-		this.prevY = 0;
-		this.currY = 0;
-		this.dot_flag = false;
+		this.drawing = false;
 		this.ctx = null;
-		this.x = 'black';
-		this.y = 2;
 	}
 
-	draw() {
+	draw(lastX, lastY, currentX, currentY) {
 		this.ctx.beginPath();
-		this.ctx.moveTo(this.prevX, this.prevY);
-		this.ctx.lineTo(this.currX, this.currY);
-		this.ctx.strokeStyle = this.x;
-		this.ctx.lineWidth = this.y;
+		this.ctx.moveTo(lastX, lastY);
+		this.ctx.lineTo(currentX, currentY);
 		this.ctx.stroke();
-		this.ctx.closePath();
-	}
-
-	erase() {
-		if (this.m) {
-			this.ctx.clearRect(0, 0, this.w, this.h);
-			this.refs.canvas.style.display = "none";
-		}
 	}
 
 	findxy(res, e) {
 		if (res == 'down') {
-			console.log(e.clientX);
-			console.log(e.clientY);
-			this.prevX = this.currX;
-			this.prevY = this.currY;
-			this.currX = e.clientX - this.refs.canvas.offsetLeft;
-			this.currY = e.clientY - this.refs.canvas.offsetTop;
-
-			this.flag = true;
-			this.dot_flag = true;
-			if (this.dot_flag) {
-				this.ctx.beginPath();
-				this.ctx.fillStyle = this.x;
-				this.ctx.fillRect(this.currX, this.currY, 2, 2);
-				this.ctx.closePath();
-				this.dot_flag = false;
-			}
-		}
-		if (res == 'up' || res == "out") {
-			this.flag = false;
-		}
-		if (res == 'move') {
-			if (this.flag) {
-				this.prevX = this.currX;
-				this.prevY = this.currY;
-				this.currX = e.clientX - this.refs.canvas.offsetLeft;
-				this.currY = e.clientY - this.refs.canvas.offsetTop;
-				this.draw();
+			this.lastX = e.clientX - this.refs.canvas.offsetLeft;
+			this.lastY = e.clientY - this.refs.canvas.offsetTop;
+			this.drawing = true;
+		} else if (res == 'up' || res == "out") {
+			this.drawing = false;
+		} else if (res == 'move') {
+			if (this.drawing) {
+				this.draw(this.lastX, this.lastY, e.clientX - this.refs.canvas.offsetLeft, e.clientY - this.refs.canvas.offsetTop);
+				this.lastX = e.clientX - this.refs.canvas.offsetLeft;
+				this.lastY = e.clientY - this.refs.canvas.offsetTop;
 			}
 		}
 	}
 
-	componentDidMount() {	
+	componentDidMount() {
 		this.ctx = this.refs.canvas.getContext('2d');
-		this.w = this.refs.canvas.width;
-		this.h = this.refs.canvas.height;
 	}
 
 	render() {
-
 		return (
 			<div>
-				<Paper elevation={4}>
-					<canvas id="board" 
+				<Paper ref="canvasContainer" elevation={4}>
+					<canvas id="board"
 						ref="canvas"
-						style={boardStyle} 
 						className="board"
+						width={500}
+						height={500}
 						onMouseDown={(e) => this.findxy('down', e)}
 						onMouseMove={(e) => this.findxy('move', e)}
 						onMouseUp={(e) => this.findxy('up', e)}
@@ -111,8 +70,4 @@ class WhiteBoard extends React.Component {
 	}
 }
 
-WhiteBoard.propTypes = {
-	classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(WhiteBoard);
+export default (WhiteBoard);
